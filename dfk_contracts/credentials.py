9b7180f7521da8bc56ed65265890dc25d2f093bb
Athiriyya <athiriyya@gmail.com>
2023-03-31 16:30:27 -0500

    Add support for finding Credentials via abbreviations, where an abbreviation is a case-insensitive string matching the beginning of a Credential's nickname (e..g. 'sli' for 'Slipfoot')

--- dfk_contracts/credentials.py
+++ dfk_contracts/credentials.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python
 
 from .solidity_types import address as HexAddress
-from typing import Dict, List, Optional, Sequence, Tuple, Union
+from typing import Dict, List, Optional, Sequence, Tuple, Union, cast
 
 class Credentials:
-    def __init__(self, address:HexAddress, private_key:str ='', nickname=''):
-        self.address = address
+    def __init__(self, address:HexAddress|str, private_key:str ='', nickname:str=''):
+        self.address:HexAddress = cast(HexAddress, address)
         self.private_key = private_key
         self.nickname = nickname
 
@@ -15,9 +15,17 @@ class Credentials:
     def __repr__(self):
         return f'{self.nickname:<10}:  {self.address}'
 
+    def matches_abbreviation(self, query:str) -> bool:
+        # An 'abbreviation' is a string that has the same beginning 
+        # as self.nickname, case-insensitive, e.g. 'sli' for 'Slipfoot'
+        return self.nickname.lower().startswith(query.lower())
+
     @staticmethod
-    def cred_for_nickname(creds:Sequence['Credentials'], nickname:str) -> 'Credentials':
-        gen = (c for c in creds if c.nickname == nickname)
+    def cred_for_nickname(creds:Sequence['Credentials'], nickname:str, accept_abbreviation=True) -> 'Credentials':
+        if accept_abbreviation:
+            gen = (c for c in creds if c.matches_abbreviation(nickname))
+        else:
+            gen = (c for c in creds if c.nickname == nickname)
         try:
             return next(gen)
         except StopIteration:
